58f3a50d2e4cbdffa77d80008d587a4b607f040f
10-biter <ep.student@hotmail.com>
2025-11-25 15:36:50 +0300

    Initial files for MakeCode project

---
+++ custom.ts
@@ -0,0 +1,20 @@
+
+/**
+ * Custom blocks
+ */
+//% weight=100 color=#1c1f78 icon="\f236"
+//% weight=100 color=#1c1f78 icon=""
+namespace spriteSleep {
+    /**
+     * TODO: describe your function here
+     * @param n describe parameter here, eg: 5
+     * @param s describe parameter here, eg: "Hello"
+     * @param e describe parameter here
+     */
+    //% block
+    export function sleep(sprite: Sprite): void {
+        controller.moveSprite(sprite, 0, 0)
+        sprite.setFlag(SpriteFlag.Ghost, true)
+
+    }
+}
